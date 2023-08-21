5057dfb6f67beaab9e4a7f6f88e1b8b4b8ac365c
rsrahman000 <90463352+rsrahman000@users.noreply.github.com>
2023-08-21 16:51:09 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/Public'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'teamdbcs777@gmail.com',
@@ -50,11 +50,11 @@ module.exports = {
   links: [
     {
       name: 'Website',
-      link: 'https://www.teamdbcs.gq',
+      link: 'https://teamdbcs.com',
     },
     {
-      name: 'Telegram',
-      link: 'http://dc.teamdbcs.gq',
+      name: 'Discord',
+      link: 'https://dicord.teamdbcs.com',
     },
   ],
 
